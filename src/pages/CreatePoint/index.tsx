8a503c9abc9d472d7509da40c3a9ebacf43f1c05
recofka <recofka@gmail.com>
2020-06-05 23:16:22 +0200

    organizing data in local state and sending to the API

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
@@ -17,15 +17,28 @@ interface Item {
 const CreatePoint = () => {
     const [items, setItems] = useState<Item[]>([]);
     const neighborhood = ['Zeeburgereiland', 'Ijburg', 'Zeeburg'];
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+    });
     // neighborhood = nbhd
     const [selectedNbhd, setSelectedNbhd] = useState('0');
-    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+    const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+    const [selectedItems, setSelectedItems] = useState<number[]>([]);
 
     useEffect(() => {
         api.get('items').then(response => {
             setItems(response.data);
         })
     }, [])
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+            setInitialPosition([latitude, longitude]);
+        });
+    }, []);
 
     function handleSelectNbhd(event: ChangeEvent<HTMLSelectElement>) {
         const neighborhood = event.target.value;
@@ -39,6 +52,40 @@ const CreatePoint = () => {
         ]);
     }
 
+    function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    function handleSelectItem(id: number) {
+        const alredySelected = selectedItems.findIndex(item => item === id);
+
+        if (alredySelected > 0) {
+            const filteredItems = selectedItems.filter(item => item !== id);
+            setSelectedItems(filteredItems)
+        } else {
+            setSelectedItems([...selectedItems, id]);
+        }
+
+    };
+
+    async function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+        const { name, email } = formData;
+        const city = selectedNbhd;
+        const [latitude, longitude] = selectedPosition;
+        const items = selectedItems;
+        const data = {
+            name,
+            email,
+            latitude,
+            longitude,
+            city,
+            items
+        };
+        await api.post('points', data);
+    };
+
     return (
         <div id="page-create-point">
             <header >
@@ -49,7 +96,7 @@ const CreatePoint = () => {
                 </Link>
             </header>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Register a <br /> collection point</h1>
 
                 <fieldset>
@@ -60,12 +107,22 @@ const CreatePoint = () => {
 
                 <div className="field">
                     <label htmlFor="name">Point name</label>
-                    <input type="text" name="name" id="name" />
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        onChange={handleInputChange}
+                    />
                 </div>
 
                 <div className="field">
                     <label htmlFor="email">E-mail</label>
-                    <input type="email" name="email" id="email" />
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        onChange={handleInputChange}
+                    />
                 </div>
 
                 <fieldset>
@@ -75,7 +132,7 @@ const CreatePoint = () => {
                     </legend>
                 </fieldset>
 
-                <Map center={[52.372467, 4.963344]} zoom={15} onClick={handleMapClick}>
+                <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -107,7 +164,11 @@ const CreatePoint = () => {
                     </legend>
                     <ul className="items-grid">
                         {items.map(item => (
-                            <li key={item.id}>
+                            <li
+                                key={item.id}
+                                onClick={() => handleSelectItem(item.id)}
+                                className={selectedItems.includes(item.id) ? 'selected' : ''}
+                            >
                                 <img src={item.image_url} alt={item.title} />
                                 <span>{item.title}</span>
                             </li>
