786fc78a43e8e3f827834eeb497703f77643e0ce
recofka <recofka@gmail.com>
2020-06-08 20:10:06 +0200

    Split header in a component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
+import Header from '../../components/Header';
 import { FiLogIn } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import './styles.css'
-import logo from '../../assets/logo.svg'
 
 const Home = () => {
     return (
         <div id="page-home">
             <div className="content">
-                <header>
-                    <img src={logo} alt="Logo" />
-                </header>
+                <Header />
                 <main>
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiArrowLeft, FiCheckCircle } from 'react-icons/fi';
-import logo from '../../assets/logo.svg';
+import Header from '../../components/Header';
+import { FiCheckCircle } from 'react-icons/fi';
+
 import './styles.css';
 
 const SuccessCreatePoint = () => {
     return (
         <div id="page-create-point-success">
-            <header >
-                <img src={logo} alt="Logo" />
-                <Link to="/">
-                    <FiArrowLeft />
-                    Back to Home
-                </Link>
-            </header>
+            <Header />
+
             <div className="success">
                 <FiCheckCircle className="icon" />
                 <h1>Success!</h1>
